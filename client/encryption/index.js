4f83ac192f500b1cfdd0a9f05e8514cb716e7d35
aggregate1166877 <noreply@frostoven.com>
2023-11-20 02:47:17 +1300

    Make encryption wrapper RSA-focussed.

--- client/encryption/index.js
+++ client/encryption/index.js
@@ -7,48 +7,32 @@ import * as WebCrypto from 'easy-web-crypto';
 
 /**
  * Creates and returns the specified types of key pairs.
- * @param {Array<string>} types - An array of types of key pairs to generate.
- * Default is [ 'rsa' ]. [ 'ecdsa' ] is supported as well.
+ * @param {string} type - Type of key pair to generate. Default is
+ * 'rsa'; 'ecdsa' is supported but not currently used.
  * @returns {Promise<{rsa, ecdsa}>}
  */
-async function createKeyPairs(types = [ 'rsa' ]) {
-  const result = {};
-  types.includes('rsa') && (result.rsa = await generateRsaKey());
-  types.includes('ecdsa') && (result.ecdsa = await WebCrypto.genKeyPair());
-  return result;
+async function createKeyPairs(type = 'rsa') {
+  if (type === 'rsa') {
+    return await generateRsaKey();
+  }
+  else {
+    return await WebCrypto.genKeyPair();
+  }
 }
 
 /**
- * Exports RSA and ECDSA keys.
- * @param {Object} keysPairs An object that contains rsa and ecdsa keys.
- * @param {Object} [keysPairs.rsa] RSA key pair.
- * @param {Object} [keysPairs.ecdsa] ECDSA key pair.
- * @param {string} [format='raw'] A string that indicates the format of
- * exporting. Default value is 'raw'.
+ * Exports RSA key pairs.
+ * @param keyPair
+ * @param {string} format - Raw or string.
  * @returns {Promise<{rsa:{private, public}, ecdsa:{private, public}}>}
  */
-async function exportKeys({ rsa, ecdsa } = {}, format = 'raw') {
-  const result = {};
-
-  if (rsa) {
-    const publicRsaKeyBuffer = await exportRsaPublicKey(rsa, 'raw');
-    const privateRsaKeyBuffer = await exportRsaPrivateKey(rsa, 'raw');
-    result.rsa = {
-      public: publicRsaKeyBuffer,
-      private: privateRsaKeyBuffer,
-    };
-  }
-
-  if (ecdsa) {
-    const publicEcdsaKeyBuffer = await WebCrypto.exportPublicKey(ecdsa.publicKey, 'raw');
-    const privateEcdsaKeyBuffer = await WebCrypto.exportPrivateKey(ecdsa.privateKey, 'raw');
-    result.ecdsa = {
-      public: publicEcdsaKeyBuffer,
-      private: privateEcdsaKeyBuffer,
-    };
-  }
-
-  return result;
+async function exportKeys(keyPair, format = 'raw') {
+  const publicRsaKeyBuffer = await exportRsaPublicKey(keyPair, format);
+  const privateRsaKeyBuffer = await exportRsaPrivateKey(keyPair, format);
+  return {
+    public: publicRsaKeyBuffer,
+    private: privateRsaKeyBuffer,
+  };
 }
 
 export {
