410dbf93de98e8604fb1aeb76212e1d71aad5717
aggregate1166877 <noreply@frostoven.com>
2023-11-21 08:14:01 +1300

    Change CreateFirstChat so that it uses LargeCircleIconButton.

--- client/components/ChatCreator/CreateFirstChat.js
+++ client/components/ChatCreator/CreateFirstChat.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Button, Icon } from 'semantic-ui-react';
+import { LargeCircleIconButton } from '../Generic/LargeCircleIconButton';
 
 class CreateFirstChat extends React.Component {
+  createChat = () => {
+    //
+  };
+
   render() {
     return (
-      <Button style={{ textAlign: 'center', width: 200, height: 200, borderRadius: '100%' }}>
-        <Icon size='big' name="pencil alternate" style={{ paddingLeft: 11 }}/>
-        <br/><br/><br/>
-        <b>Create your first chat</b>
-      </Button>
+      <LargeCircleIconButton
+        icon="pencil alternate"
+        label="Create your first chat"
+      />
     );
   }
 }
