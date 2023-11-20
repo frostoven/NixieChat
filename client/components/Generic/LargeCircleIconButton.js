3ccf8e6d850d07a6c67b18d97aede7e165fa7859
aggregate1166877 <noreply@frostoven.com>
2023-11-21 08:13:23 +1300

    Add LargeCircleIconButton component.

---
+++ client/components/Generic/LargeCircleIconButton.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Icon } from 'semantic-ui-react';
+
+class LargeCircleIconButton extends React.Component {
+  static propTypes = {
+    onClick: PropTypes.func,
+    icon: PropTypes.string,
+    label: PropTypes.string,
+  };
+
+  static defaultProps = {
+    onClick: () => {
+      console.warn('[LargeCircleIconButton] no callback specified.');
+    },
+    icon: 'paperclip',
+    label: 'Button',
+  };
+
+  render() {
+    return (
+      <div onClick={this.createChat} style={{
+        cursor: 'pointer',
+        textAlign: 'center',
+        width: 200,
+        height: 200,
+        margin: 'auto',
+      }}>
+        <br/>
+        <Button style={{
+          textAlign: 'center',
+          width: 99,
+          height: 99,
+          borderRadius: '100%',
+        }}>
+          <Icon
+            size="big"
+            /** @type  */
+            name={this.props.icon}
+            style={{ paddingLeft: 12 }}
+          />
+        </Button>
+        <br/><br/>
+        <b>{this.props.label}</b>
+      </div>
+    );
+  }
+}
+
+console.log({LargeCircleIconButton})
+
+export {
+  LargeCircleIconButton,
+};
