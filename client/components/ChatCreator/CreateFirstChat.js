0a33c73234109a11a9ea59414f34de18514f6d0c
aggregate1166877 <noreply@frostoven.com>
2023-11-21 08:29:42 +1300

    Add encrypted notepad button to CreateFirstChat.

--- client/components/ChatCreator/CreateFirstChat.js
+++ client/components/ChatCreator/CreateFirstChat.js
@@ -9,10 +9,20 @@ class CreateFirstChat extends React.Component {
 
   render() {
     return (
-      <LargeCircleIconButton
-        icon="pencil alternate"
-        label="Create your first chat"
-      />
+      <div style={{ textAlign: 'center' }}>
+        <LargeCircleIconButton
+          icon="pencil alternate"
+          label="Create your first chat"
+        />
+
+        <p><b>&mdash;&nbsp;&nbsp;or&nbsp;&nbsp;&mdash;</b></p>
+
+        <LargeCircleIconButton
+          icon="sticky note"
+          label="Create encrypted notepad"
+          iconStyle={{ paddingLeft: 15 }}
+        />
+      </div>
     );
   }
 }
