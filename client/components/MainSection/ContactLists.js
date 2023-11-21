e543cb339053b102fb352f681669af5e41ca4ee0
aggregate1166877 <noreply@frostoven.com>
2023-11-21 18:30:41 +1300

    Add profile photo space to contacts.

--- client/components/MainSection/ContactLists.js
+++ client/components/MainSection/ContactLists.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const contactsListStyle = {
+  // borderBottom: 'thin solid grey',
+  paddingLeft: 2,
+  paddingTop: 12,
+  paddingBottom: 12,
+};
+
 const listStyle = {
   width: '100%',
   height: '100%',
   borderRight: 'thin solid grey',
+  overflowY: 'auto',
+};
+
+const profilePhotoContainer = {
+  display: 'inline-block',
+  verticalAlign: 'top',
+};
+
+const profileIconStyle = {
+  borderRadius: '100%',
+  backgroundColor: 'grey',
+  width: 42,
+  height: 42,
+  marginLeft: 4,
+  marginRight: 12,
+};
+
+const contactInfoContainer = {
+  display: 'inline-block'
+};
+
+const taperedLineStyle = {
+  borderRadius: '15%',
+  width: '97%',
+  backgroundColor: 'grey',
+  height: 1,
+  margin: 'auto',
 };
 
 class ContactsList extends React.Component {
@@ -19,28 +53,10 @@ class ContactsList extends React.Component {
   genList = () => {
     const contacts = [
       {
+        // Example structure.
         name: 'Name TBA',
         lastMessage: 'Last message shown here',
         photo: '',
-        online: false,
-      },
-      {
-        name: 'Name TBA',
-        lastMessage: 'Last message shown here',
-        photo: '',
-        online: false,
-      },
-      {
-        name: 'Name TBA',
-        lastMessage: 'Last message shown here',
-        photo: '',
-        online: false,
-      },
-      {
-        name: 'Name TBA',
-        lastMessage: 'Last message shown here',
-        photo: '',
-        online: false,
       },
     ];
 
@@ -49,34 +65,26 @@ class ContactsList extends React.Component {
     for (let i = 0, len = contacts.length; i < len; i++) {
       const { name, lastMessage } = contacts[i];
       contactsJsx.push(
-        <div key={`contacts-list-${i}`} style={{
-          borderBottom: 'thin solid grey',
-          paddingLeft: 2,
-          paddingTop: 12,
-          paddingBottom: 12,
-        }}>
-          <div style={{ display: 'inline-block', verticalAlign: 'top' }}>
-            <div
-              style={{
-                borderRadius: '100%',
-                backgroundColor: 'grey',
-                width: 42,
-                height: 42,
-                marginLeft: 4,
-                marginRight: 12,
-              }}
-            >
+        <div key={`contacts-list-${i}`} style={contactsListStyle}>
+
+          {/* Contact profile photo */}
+          <div style={profilePhotoContainer}>
+            <div style={profileIconStyle}>
               &nbsp;
             </div>
           </div>
-          <div
-            style={{ display: 'inline-block' }}
-          >
+
+          {/* Contact name and recent message */}
+          <div style={contactInfoContainer}>
             <div>
               <div><b>{name}</b></div>
               <div>{lastMessage}</div>
             </div>
           </div>
+
+        </div>,
+        <div style={taperedLineStyle}>
+          &nbsp;
         </div>,
       );
     }
