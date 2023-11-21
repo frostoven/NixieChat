d2d06ad1a3c1421772cd9274ab69a61691787095
aggregate1166877 <noreply@frostoven.com>
2023-11-21 13:28:22 +1300

    Add notepad button to CreateFirstContact.

--- client/components/ContactFinder/CreateFirstContact.js
+++ client/components/ContactFinder/CreateFirstContact.js
@@ -2,16 +2,30 @@ import React from 'react';
 import { LargeCircleIconButton } from '../Generic/LargeCircleIconButton';
 
 class CreateFirstContact extends React.Component {
+  createChat = () => {
+    //
+  };
+
   render() {
     return (
-      <LargeCircleIconButton
-        icon='user plus'
-        label='Add your first contact'
-      />
-    )
+      <div style={{ textAlign: 'center' }}>
+        <LargeCircleIconButton
+          icon="user plus"
+          label="Add your first contact"
+        />
+
+        <p><b>&mdash;&nbsp;&nbsp;or&nbsp;&nbsp;&mdash;</b></p>
+
+        <LargeCircleIconButton
+          icon="sticky note"
+          label="Create encrypted notepad"
+          iconStyle={{ paddingLeft: 15 }}
+        />
+      </div>
+    );
   }
 }
 
 export {
   CreateFirstContact,
-}
+};
