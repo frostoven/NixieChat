ab05f8837d0c7edc57c5385653c57535ef8d4c3b
aggregate1166877 <noreply@frostoven.com>
2023-11-23 12:40:23 +1300

    Improve chat background image size.

--- client/components/MainSection/ActiveChat.js
+++ client/components/MainSection/ActiveChat.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
 const activeChatStyle = {
-  backgroundImage: 'url(/assets/backgrounds/defaultPattern.svg)',
   padding: 8,
+  backgroundImage: 'url(/assets/backgrounds/defaultPattern.svg)',
+  backgroundSize: 392,
 };
 
 class ActiveChat extends React.Component {
   render() {
     return (
       <div style={activeChatStyle}>
-        // active chat here NG
+        // active chat
       </div>
     )
   }
