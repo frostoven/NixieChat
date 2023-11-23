228444113dcd55419dcb0ba58d3ebb825568b94c
aggregate1166877 <noreply@frostoven.com>
2023-11-24 01:23:35 +1300

    Add Settings object to interface with storage at a high level.

---
+++ client/storage/Settings.js
@@ -0,0 +1,35 @@
+import { NixieStorage } from './NixieStorage';
+import { clientEmitter } from '../emitters/comms';
+import { clientEmitterAction } from '../emitters/clientEmitterAction';
+
+const storage = new NixieStorage();
+
+class Settings {
+  static isDarkModeEnabled() {
+    const cache = storage.settingsCache || {};
+    return cache.darkModeEnabled || false;
+  }
+
+  static async setDarkModeEnabled(value, updateUi) {
+    const cache = storage.settingsCache;
+    cache.darkModeEnabled = value;
+    await storage.writeSettings(cache);
+    if (updateUi) {
+      clientEmitter.emit(clientEmitterAction.hardReloadApp);
+    }
+  }
+
+  static async toggleDarkMode(updateUi) {
+    const cache = storage.settingsCache;
+    const darkMode = cache.darkModeEnabled;
+    cache.darkModeEnabled = !darkMode;
+    await storage.writeSettings(storage.settingsCache);
+    if (updateUi) {
+      clientEmitter.emit(clientEmitterAction.hardReloadApp);
+    }
+  }
+}
+
+export {
+  Settings,
+};
