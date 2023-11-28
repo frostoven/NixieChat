64368730400c7e64d81b20556d0a0088da8649dc
aggregate1166877 <noreply@frostoven.com>
2023-11-28 20:51:44 +1300

    Add getSafeRandomIntInclusive function.

--- client/utils.js
+++ client/utils.js
@@ -8,7 +8,20 @@ function arrayBufferToString(ab) {
   return new Uint8Array(ab).reduce((p, c) => p + String.fromCharCode(c), '');
 }
 
+// This function under the MIT license. Taken from:
+// https://github.com/chancejs/chancejs/issues/232#issuecomment-182500222
+// https://stackoverflow.com/questions/18230217/javascript-generate-a-random-number-within-a-range-using-crypto-getrandomvalues
+function getSafeRandomIntInclusive(min, max) {
+  const randomBuffer = new Uint32Array(1);
+  window.crypto.getRandomValues(randomBuffer);
+  let randomNumber = randomBuffer[0] / (0xffffffff + 1);
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(randomNumber * (max - min + 1)) + min;
+}
+
 export {
   stringToArrayBuffer,
   arrayBufferToString,
+  getSafeRandomIntInclusive,
 };
