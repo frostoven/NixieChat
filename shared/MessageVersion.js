b5daf1356cf55c145d5f3ba719e69cd0ae6eef3a
aggregate1166877 <noreply@frostoven.com>
2023-12-12 16:02:58 +1300

    Initial MessageVersion code.

---
+++ shared/MessageVersion.js
@@ -0,0 +1,12 @@
+// Really just here for future compatibility, represents the authentication
+// version / type for whatever crypto exchange is happening.
+const MessageVersion = {
+  discoverableRequestV1: 'v1',
+  deviceRegistrationV1: 'v1',
+  messageExchangeV1: 'v1',
+  findContactV1: 'v1',
+};
+
+module.exports = {
+  MessageVersion: MessageVersion,
+};
