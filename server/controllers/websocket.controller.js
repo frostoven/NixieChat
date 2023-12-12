371c073d869fda586533e50ac89cbc71c11e2160
aggregate1166877 <noreply@frostoven.com>
2023-12-12 16:45:50 +1300

    Add server-side makeDiscoverable function.

--- server/controllers/websocket.controller.js
+++ server/controllers/websocket.controller.js
@@ -2,13 +2,21 @@ const { generateEventSignaller } = require('../socketProcessing/generateEventSig
 
 const socketEvent = {
   ping: generateEventSignaller(),
+  makeDiscoverable: generateEventSignaller(),
 };
 
 function ping(socket) {
   socketEvent.ping.trigger({ socket });
 }
 
+function makeDiscoverable(socket, options, callback) {
+  if (socket) {
+    socketEvent.makeDiscoverable.trigger({ socket, options, callback });
+  }
+}
+
 module.exports = {
   socketEvent,
   ping,
+  makeDiscoverable,
 };
--- server/socketProcessing/index.js
+++ server/socketProcessing/index.js
@@ -9,6 +9,64 @@ function bootServer() {
       message: 'Received.', socket,
     });
   });
+  socketEvent.ping.addListener(
+    /**
+     * @param {Object} payload
+     * @param {Socket} payload.socket
+     * @param {Object} payload.options
+     * @param {Function} payload.callback
+     */
+    ({ socket, options = {}, callback = nop } = {}) => {
+      return sendMessageToClient({
+        message: 'Received.', socket,
+      });
+    });
+
+  /**
+   * @param {Object} payload
+   * @param {Socket} payload.socket
+   * @param {Object} payload.options
+   * @param {Function} payload.callback
+   */
+  socketEvent.makeDiscoverable.addListener(
+    /**
+     * @param {Object} payload
+     * @param {Socket} payload.socket
+     * @param {Object} payload.options
+     * @param {Function} payload.callback
+     */
+    ({ socket, options = {}, callback = nop } = {}) => {
+      const { userRooms, v } = options;
+      console.log(`[makeDiscoverable] socket.id: ${socket.id}, v: ${v}, userRooms:`, userRooms);
+      if (
+        v !== MessageVersion.messageExchangeV1 ||
+        !Array.isArray(userRooms)
+      ) {
+        callback({
+          error: '[makeDiscoverable] Unsupported options or version.',
+        });
+        return;
+      }
+
+      if (!socket.data.pubNames) {
+        socket.data.pubNames = {};
+      }
+
+      const rejected = [];
+      for (let i = 0, len = userRooms.length; i < len; i++) {
+        const pubName = userRooms[i].pubName;
+
+        if (typeof pubName !== 'string' || pubName.length < 5 || pubName.length > 36) {
+          rejected.push(i);
+          continue;
+        }
+
+        console.log('-> Client joins invite room', pubName);
+        socket.join(pubName);
+      }
+
+      callback({ rejected });
+    });
 }
 
 // --- Exports --- //
