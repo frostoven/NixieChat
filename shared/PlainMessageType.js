bba59b13304d1e3e94145a2581f79aea5648dfee
aggregate1166877 <noreply@frostoven.com>
2023-12-12 16:03:04 +1300

    Initial PlainMessageType code.

---
+++ shared/PlainMessageType.js
@@ -0,0 +1,11 @@
+const PlainMessageType = {
+  nop: 0,
+  // Signature: { tag: Uint8Array, publicKey: CryptoKey }
+  error: 1,
+  message: 2,
+  notifyServerReady: 3,
+};
+
+module.exports = {
+  PlainMessageType,
+};
