ca2d491480a8886946a1f74784376b4516fdf3b9
aggregate1166877 <noreply@frostoven.com>
2023-12-12 17:09:51 +1300

    Initial ReceiveInvitation component code.

---
+++ client/components/ContactComponents/ReceiveInvitation.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Accordion, Button, Icon, Segment } from 'semantic-ui-react';
+import { Settings } from '../../storage/cacheFrontends/Settings';
+
+class ReceiveInvitation extends React.Component {
+  static propTypes = {
+    dialog: PropTypes.object.isRequired,
+    source: PropTypes.string.isRequired,
+    ownName: PropTypes.string.isRequired,
+    greeting: PropTypes.string.isRequired,
+    pubKey: PropTypes.any.isRequired,
+    time: PropTypes.number.isRequired,
+  };
+
+  state = {
+    showAdvancedInfo: false,
+  };
+
+  actions = [
+    {
+      name: 'Reject Invite',
+      onSelect: $modal.deactivateModal,
+      disabled: true,
+    },
+    {
+      name: 'Block Account',
+      onSelect: $modal.deactivateModal,
+      disabled: true,
+    },
+    {
+      name: 'Accept Invite',
+      onSelect: $modal.deactivateModal,
+      disabled: true,
+    },
+  ];
+
+  componentDidMount() {
+    const dialog = this.props.dialog;
+    dialog.actions = [
+      ...this.actions,
+      <Button key="invite" disabled
+              style={{ position: 'absolute', left: 4 }}>
+        <i>Options available in 3 seconds</i>
+      </Button>,
+    ];
+
+    // Force modal to recognize button changes.
+    $modal.invalidate();
+
+    // Wait 3 seconds, then make the buttons clickable. We wait to ensure the
+    // user doesn't accidentally click any buttons while using the app.
+    setTimeout(() => {
+      this.actions.forEach((action) => {
+        action.disabled = false;
+      });
+      this.props.dialog.actions = this.actions;
+      $modal.invalidate();
+    }, 3000);
+  }
+
+  toggleAdvancedInfo = () => {
+    this.setState({ showAdvancedInfo: !this.state.showAdvancedInfo });
+  };
+
+  genRsaPreview = ({ visible }) => {
+  };
+
+  render() {
+    const { showAdvancedInfo } = this.state;
+    const { source, ownName } = this.props;
+    const darkMode = Settings.isDarkModeEnabled();
+
+    return (
+      <div>
+        You have received a contact invite from someone claiming to be
+        "{source}". This request was sent to "{ownName}".
+        <br/><br/>
+        If you are not expecting an invitation, it is highly advisable you
+        decline this request. Only accept contacts you trust; they can resend
+        the invitation at any time (unless you block them).
+        <br/><br/>
+        This invite expires in 120 seconds.
+
+        <Segment inverted={!darkMode}>
+          <Accordion inverted={!darkMode}>
+            <Accordion.Title
+              active={showAdvancedInfo}
+              onClick={this.toggleAdvancedInfo}
+            >
+              <Icon name="dropdown"/>
+              Advanced Info
+            </Accordion.Title>
+            <Accordion.Content active={showAdvancedInfo}>
+              The prospective contact's RSA-4096 digital signature is as follows:
+              <br/>
+              {this.genRsaPreview({ visible: showAdvancedInfo })}
+            </Accordion.Content>
+          </Accordion>
+        </Segment>
+      </div>
+    );
+  }
+}
+
+export {
+  ReceiveInvitation,
+};
