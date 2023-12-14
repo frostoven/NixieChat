d7c868a1371afbce1d865010a697d471c86d7738
aggregate1166877 <noreply@frostoven.com>
2023-12-14 19:20:49 +1300

    Move invitation dialog to nixieDialogs.

--- client/api/RemoteCrypto.js
+++ client/api/RemoteCrypto.js
@@ -7,6 +7,7 @@ import { exportRsaPublicKey, importRsaPublicKey } from '../encryption/rsa';
 import {
   ReceiveInvitation,
 } from '../components/ContactComponents/ReceiveInvitation';
+import { showInvitationDialog } from '../modal/nixieDialogs';
 
 const nop = () => {
 };
@@ -120,71 +121,19 @@ class RemoteCrypto {
     greeting,
     pubKey,
     time,
-  } = {}) {
+  } = {}, serverCallback) {
     console.log({ requestId, source, greeting });
     // TODO: toast that invite was received, in case user is in another modal.
 
-    if (
-      typeof requestId !== 'string' ||
-      typeof source !== 'string' ||
-      typeof greeting !== 'string' ||
-      !(pubKey instanceof ArrayBuffer) ||
-      typeof time !== 'number'
-    ) {
-      console.log(
-        '[receiveInvite] Received malformed invite. Dump:',
-        { requestId, source, greeting, pubKey, time },
-      );
-      return;
-    }
-
-    // Node sends this as an ArrayBuffer, so we wrap it in a uint8 view.
-    pubKey = new Uint8Array(pubKey);
-
-    // Useful for visualisations.
-    let pemKey = await importRsaPublicKey(pubKey, 'raw');
-    pemKey = await exportRsaPublicKey({ publicKey: pemKey }, 'pem');
-
-    const dialog = $modal.alert({
-      header: 'Contact Invite',
-      body: 'Loading invite...',
-      hideStackCounter: true,
+    console.log('[invite]', {
+      ownName, requestId, source, greeting, pubKey, time,
     });
 
-    dialog.onDimmerClick = () => {
-      // Prevent accidentally closing invitations.
-      const confirmation = $modal.confirm({
-        header: 'Invite',
-        body: 'Are you sure you want to reject the invite?',
-        prioritise: true,
-        hideStackCounter: true,
-      }, (proceed) => {
-        if (proceed) {
-          $modal.deactivateModalById(confirmation.id);
-          $modal.deactivateModalById(dialog.id);
-        }
-        else {
-          $modal.deactivateModalById(confirmation.id);
-        }
-      });
-    };
+    const response = await showInvitationDialog({
+      ownName, requestId, source, greeting, pubKey, time,
+    });
 
-    // We create this after $modal.alert() so that we can safely pass in the
-    // dialog options object.
-    dialog.body = (
-      <ReceiveInvitation
-        dialog={dialog}
-        source={source}
-        ownName={ownName}
-        greeting={greeting}
-        pubKey={pubKey}
-        pemKey={pemKey}
-        time={time}
-      />
-    );
-
-    // Force the dialog to recognise the body change.
-    $modal.invalidate();
+    // serverCallback(response);
   }
 }
 
--- client/modal/nixieDialogs.js
+++ client/modal/nixieDialogs.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { ContactFinder } from '../components/ContactComponents/ContactFinder';
+import { exportRsaPublicKey, importRsaPublicKey } from '../encryption/rsa';
+import {
+  ReceiveInvitation,
+} from '../components/ContactComponents/ReceiveInvitation';
 
 function showAddContactDialog() {
   $modal.alert({
@@ -9,6 +13,81 @@ function showAddContactDialog() {
   });
 }
 
+async function showInvitationDialog({
+  ownName,
+  requestId,
+  source,
+  greeting,
+  pubKey,
+  time,
+} = {}) {
+  if (
+    typeof requestId !== 'string' ||
+    typeof source !== 'string' ||
+    typeof greeting !== 'string' ||
+    !(pubKey instanceof ArrayBuffer) ||
+    typeof time !== 'number'
+  ) {
+    console.log(
+      '[receiveInvite] Received malformed invite. Dump:',
+      { requestId, source, greeting, pubKey, time },
+    );
+    return;
+  }
+
+  // Node sends this as an ArrayBuffer, so we wrap it in a uint8 view.
+  pubKey = new Uint8Array(pubKey);
+
+  // Useful for visualisations.
+  let pemKey = await importRsaPublicKey(pubKey, 'raw');
+  pemKey = await exportRsaPublicKey({ publicKey: pemKey }, 'pem');
+
+  const dialog = $modal.alert({
+    header: 'Contact Invite',
+    body: 'Loading invite...',
+    hideStackCounter: true,
+  });
+
+  dialog.onDimmerClick = () => {
+    // Prevent accidentally closing invitations.
+    const confirmation = $modal.confirm({
+      header: 'Invite',
+      body: 'Are you sure you want to reject the invite?',
+      prioritise: true,
+      hideStackCounter: true,
+    }, (proceed) => {
+      if (proceed) {
+        $modal.deactivateModalById(confirmation.id);
+        $modal.deactivateModalById(dialog.id);
+      }
+      else {
+        $modal.deactivateModalById(confirmation.id);
+      }
+    });
+  };
+
+  return new Promise((resolve) => {
+    // We create this after $modal.alert() so that we can safely pass in the
+    // dialog options object.
+    dialog.body = (
+      <ReceiveInvitation
+        dialog={dialog}
+        source={source}
+        ownName={ownName}
+        greeting={greeting}
+        pubKey={pubKey}
+        pemKey={pemKey}
+        time={time}
+        onSelectChoice={resolve}
+      />
+    );
+
+    // Force the dialog to recognise the body change.
+    $modal.invalidate();
+  });
+}
+
 export {
   showAddContactDialog,
+  showInvitationDialog,
 };
