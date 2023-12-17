762fa9e82ce31196db397436a7e785aaf87c014d
aggregate1166877 <noreply@frostoven.com>
2023-12-18 10:23:29 +1300

    Add respondToInvite type to CryptoMessageType.

--- shared/CryptoMessageType.js
+++ shared/CryptoMessageType.js
@@ -4,6 +4,7 @@ const CryptoMessageType = {
   makeDiscoverable: 'makeDiscoverable',
   // Signature: { source, target, greeting, pubKey, time, v }
   findContact: 'findContact',
+  respondToInvite: 'respondToInvite',
 };
 
 module.exports = {
