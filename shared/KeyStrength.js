8a7dea493ba05925b98410476cca9912d1ef96fd
aggregate1166877 <noreply@frostoven.com>
2023-12-18 10:22:56 +1300

    Add KeyStrength file.

---
+++ shared/KeyStrength.js
@@ -0,0 +1,9 @@
+// https://nodejs.org/api/crypto.html#class-diffiehellmangroup
+const KeyStrength = {
+  // modp18 is 8192 bits.
+  messagingModGroup: 'modp18',
+};
+
+module.exports = {
+  KeyStrength,
+};
