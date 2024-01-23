82fbd3f3d149e5dc57387938f221124d185a26d4
aggregate1166877 <noreply@frostoven.com>
2024-01-23 16:27:13 +1300

    [Partial] Add findAccountById function.

--- client/storage/cacheFrontends/Accounts.js
+++ client/storage/cacheFrontends/Accounts.js
@@ -4,6 +4,20 @@ import { clientEmitterAction } from '../../emitters/clientEmitterAction';
 
 const storage = new NixieStorage();
 
+/**
+ * Stored account.
+ *
+ * @typedef {{
+ *   accountName: string,
+ *   accountId: string,
+ *   personalName: string,
+ *   publicName: string,
+ *   contacts: CollectionCache,
+ *   privateKey: CryptoKey,
+ *   publicKey: CryptoKey
+ * }} Account
+ */
+
 class Accounts {
   /**
    * @return {CollectionCache}
@@ -12,11 +26,16 @@ class Accounts {
     return storage.accountCollectionCache;
   }
 
-  // Returns the first local account matching the specified public name, or
-  // null if the requested name does not exist.
+  /**
+   * Returns the first local account matching the specified public name, or
+   * null if the requested name does not exist.
+   * @param {string} publicName
+   * @return {Account|null}
+   */
   static findAccountByPublicName({ publicName }) {
     const accounts = Accounts.getAccountCollection().asArray;
     for (let i = 0, len = accounts.length; i < len; i++) {
+      /** @type Account */
       const account = accounts[i];
       if (account.publicName === publicName) {
         return account;
@@ -26,14 +45,25 @@ class Accounts {
   }
 
   /**
-   * @returns {{
-   *   accountName: string,
-   *   personalName: string,
-   *   publicName: string,
-   *   contacts: CollectionCache,
-   *   privateKey: CryptoKey,
-   *   publicKey: CryptoKey
-   * }|null}
+   * Returns the first local account matching the specified id, or null if the
+   * requested name does not exist.
+   * @param {string} id
+   * @return {Account|null}
+   */
+  static findAccountById({ id }) {
+    const accounts = Accounts.getAccountCollection().asArray;
+    for (let i = 0, len = accounts.length; i < len; i++) {
+      /** @type Account */
+      const account = accounts[i];
+      if (account.accountId === id) {
+        return account;
+      }
+    }
+    return null;
+  }
+
+  /**
+   * @returns {Account|null}
    */
   static getActiveAccount() {
     const lastActiveAccount = storage.lastActiveAccount;
