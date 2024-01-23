6d412c635cbf901000407b243affe1fa85e9e9ed
aggregate1166877 <noreply@frostoven.com>
2024-01-23 16:27:42 +1300

    [Partial] Update clientEmitterAction to support new emitter calls.

--- client/emitters/clientEmitterAction.js
+++ client/emitters/clientEmitterAction.js
@@ -1,9 +1,17 @@
 let _autoCounter = 0;
 
 const clientEmitterAction = {
+  // Exists only so that we can write things like "if (!validAction)".
   nop: _autoCounter++,
   softReloadApp: _autoCounter++,
   hardReloadApp: _autoCounter++,
+  // Action indicating that some ContactCreator instance has had significant
+  // changes. Used by UIs to rerender themselves if they're concerned with
+  // adding new contacts and need live updates.
+  updateContactCreatorViews: _autoCounter++,
+  // Used for DH and handshake net status updates. Contains the step being run
+  // and the overall progress percentage.
+  updateDhStatus: _autoCounter++,
 };
 
 export {
