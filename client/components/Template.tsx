e4c01192925af52b95dbc28e283c994dba150ec3
aggregate1166877 <noreply@frostoven.com>
2024-01-26 17:12:09 +1300

    Update template components.

--- client/components/Template.tsx
+++ client/components/Template.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-class Template extends React.Component {
-  static propTypes = {
-    //
-  };
+interface Props {
+}
 
-  static defaultProps = {
+class Template extends React.Component<Props> {
+  state = {
     //
   };
 
