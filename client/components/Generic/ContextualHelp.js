e2d9b864897438033c8f2cc663e0248b11dcd0f8
aggregate1166877 <noreply@frostoven.com>
2024-02-15 01:09:50 +1300

    Improve ContextualHelp dark mode support.

--- client/components/Generic/ContextualHelp.js
+++ client/components/Generic/ContextualHelp.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Icon, Popup } from 'semantic-ui-react';
+import { Settings } from '../../storage/cacheFrontends/Settings';
 
 class ContextualHelp extends React.Component {
   static propTypes = {
@@ -12,8 +13,10 @@ class ContextualHelp extends React.Component {
   };
 
   render() {
+    const darkMode = Settings.isDarkModeEnabled();
     return (
       <Popup
+        inverted={!darkMode}
         trigger={
           <Icon name="question" color="green" size="small" inverted circular/>
         }
