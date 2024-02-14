9951008e90a9b2d0833e5f6b50b0496ee412c949
aggregate1166877 <noreply@frostoven.com>
2024-02-15 03:52:00 +1300

    Wait for window load before creating the React root node.

--- client/index.js
+++ client/index.js
@@ -3,24 +3,6 @@ import ReactDOM from 'react-dom';
 import './modal';
 import { RootNode } from './RootNode';
 
-/**
- * TODO list:
- *  * create a login screen
- *  * allow creation of accounts in the login screen [explain they need to be backed up]
- *  * if you allow yourself to be found, you ca be searched or if they know your exact name
- *  * every chat is multi-user. new users may request all past plaintext messages from other
- *    people in the chat. if they refuse, then you need to resync plaintext from others to
- *    see new messages from exising user.
- *  * all messages are internally stored as jpg or png. if another is sent, convert to jpg
- *    or png depending on whether or not the source is lossless.
- *  * create a SHA-256 address of the person's public key.
- */
-
-// import { getDbByPlatform } from './storage/EncryptedAccountStorage/driver';
-// getDbByPlatform().then((driver) => {
-//   driver.run();
-// }).catch(console.error);
-
 if (
   location.protocol === 'http:' &&
   ![ 'localhost', '127.0.0.1' ].includes(location.hostname)
@@ -40,6 +22,12 @@ if (
   });
 }
 
-const domContainer = document.querySelector('#react-element');
-const element = React.createElement;
-ReactDOM.render(element(RootNode), domContainer);
+async function boot() {
+  const domContainer = document.querySelector('#react-element');
+  const element = React.createElement;
+  ReactDOM.render(element(RootNode), domContainer);
+}
+
+window.onload = function() {
+  boot().catch(console.error);
+};
