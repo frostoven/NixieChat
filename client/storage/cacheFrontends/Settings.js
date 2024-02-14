2888d75140b0456a23c1413344497cf33792eab6
aggregate1166877 <noreply@frostoven.com>
2024-02-15 02:05:37 +1300

    Add oneOrMoreAccountsUnencrypted to unencrypted storage.

--- client/storage/cacheFrontends/Settings.js
+++ client/storage/cacheFrontends/Settings.js
@@ -12,7 +12,7 @@ class Settings {
 
   static async setDarkModeEnabled(value, updateUi) {
     const cache = storage.settingsCache;
-    cache.darkModeEnabled = value;
+    cache.darkModeEnabled = !!value;
     await storage.writeSettings(cache);
     if (updateUi) {
       clientEmitter.emit(clientEmitterAction.reloadApp);
@@ -27,6 +27,27 @@ class Settings {
     if (updateUi) {
       clientEmitter.emit(clientEmitterAction.reloadApp);
     }
+  }
+
+  // If true, the user has chosen to have one or more accounts not use a
+  // password. Internally, to keep the code simple, NixieChat still uses '0000'
+  // as a password if the user has chosen to skip using a password. If this
+  // flag is set, the app will attempt to decrypt all accounts with '0000'
+  // during boot, only ask for password unlocks for those that fail to decrypt.
+  static oneOrMoreAccountsUnencrypted() {
+    const cache = storage.settingsCache || {};
+    return cache.oneOrMoreAccountsUnencrypted || false;
+  }
+
+  // If true, the user has chosen to have one or more accounts not use a
+  // password. Internally, to keep the code simple, NixieChat still uses '0000'
+  // as a password if the user has chosen to skip using a password. If this
+  // flag is set, the app will attempt to decrypt all accounts with '0000'
+  // during boot, only ask for password unlocks for those that fail to decrypt.
+  static async setOneOrMoreAccountsUnencrypted(value) {
+    const cache = storage.settingsCache;
+    cache.oneOrMoreAccountsUnencrypted = !!value;
+    await storage.writeSettings(cache);
   }
 }
 
