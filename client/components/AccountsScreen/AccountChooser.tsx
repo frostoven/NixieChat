94df4abb0eb04dc105777041c25149ee43e63134
aggregate1166877 <noreply@frostoven.com>
2024-02-15 01:21:03 +1300

    Create animations.css, and convert the AccountChooser component to TS.

---
+++ client/components/AccountsScreen/AccountChooser.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface Props {
+  onAccountActivated: Function,
+}
+
+class AccountChooser extends React.Component<Props> {
+
+  render() {
+    return (
+      <div className={'fadeInDown'}>
+      </div>
+    )
+  }
+}
+
+export {
+  AccountChooser,
+}
