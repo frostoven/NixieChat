4645c52d2151f8191b8aaaa323a62309bc2303b1
aggregate1166877 <noreply@frostoven.com>
2024-02-29 04:23:23 +1300

    Initial unicode/names map.

---
+++ client/emoticonConfig/name.ts
@@ -0,0 +1,32 @@
+const name = {
+  aubergine: 0x1F346,
+  brokenHeart: 0x1F494,
+  catFace: 0x1F431,
+  faceConfused: 0x1F615,
+  faceExpressionless: 0x1F611,
+  faceGrimacing: 0x1F62C,
+  faceLoudlyCrying: 0x1F62D,
+  faceNeutral: 0x1F610,
+  faceSalute: 0x1FAE1,
+  faceSlightlyFrowning: 0x1F641,
+  faceSlightlySmiling: 0x1F642,
+  faceSmilingTear: 0x1F972,
+  faceSweatSmile: 0x1F605,
+  faceTearsOfJoy: 0x1F602,
+  // Not to be confused with faceWithMonocle.
+  faceThinking: 0x1F914,
+  faceWithDiagonalMouth: 0x1FAE4,
+  // Not to be confused with faceThinking.
+  faceWithMonocle: 0x1F9D0,
+  faceWithPleadingEyes: 0x1F97A,
+  foldedHands: 0x1F64F,
+  heart: 0x2764,
+  peach: 0x1F351,
+  pistol: 0x1F52B,
+  thumbsDown: 0x1F44E,
+  thumbsUp: 0x1F44D,
+};
+
+export {
+  name,
+};
