fa76c5aef5981e379873a86305724658c3f1d2e7
aggregate1166877 <noreply@frostoven.com>
2024-02-29 04:31:27 +1300

    Map supported icons for each pack.

--- client/emoticonConfig/0-OpenMoji/list.ts
+++ client/emoticonConfig/0-OpenMoji/list.ts
@@ -1,8 +1,27 @@
+import { name } from '../name';
+
 // TODO: add unit tests that check for duplicate entries.
 const openMojiList: number[] = [
+  name.faceSlightlySmiling,
+  name.faceLoudlyCrying,
+  name.faceTearsOfJoy,
+  name.faceSweatSmile,
+  name.faceSmilingTear,
+  name.faceNeutral,
+  name.faceSlightlyFrowning,
+  name.faceWithMonocle,
+  name.faceSalute,
+  name.faceWithDiagonalMouth,
+  name.faceGrimacing,
+  name.heart,
+  name.brokenHeart,
+  name.pistol,
 ];
 
 const openMojiToneSupport: Map<number, boolean> = new Map();
+openMojiToneSupport.set(name.thumbsUp, true);
+openMojiToneSupport.set(name.thumbsDown, true);
+openMojiToneSupport.set(name.foldedHands, true);
 
 export {
   openMojiList,
--- client/emoticonConfig/1-Noto/list.ts
+++ client/emoticonConfig/1-Noto/list.ts
@@ -1,4 +1,24 @@
+import { name } from '../name';
+
 const notoList: number[] = [
+  name.faceSlightlySmiling,
+  name.faceSmilingTear,
+  name.faceLoudlyCrying,
+  name.faceWithPleadingEyes,
+  name.faceThinking,
+  name.faceTearsOfJoy,
+  name.faceSweatSmile,
+  name.faceNeutral,
+  name.faceExpressionless,
+  name.faceConfused,
+  name.faceSlightlyFrowning,
+  name.faceGrimacing,
+  name.catFace,
+  name.heart,
+  name.brokenHeart,
+  name.pistol,
+  name.aubergine,
+  name.peach,
 ];
 
 const notoToneSupport: Map<number, boolean> = new Map();
