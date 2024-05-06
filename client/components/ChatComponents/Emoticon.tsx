83828473450463391faffdd60c399368a14fe09f
aggregate1166877 <noreply@frostoven.com>
2024-05-07 11:56:58 +1200

    Update the Emoticon component to use newer codebase features.

--- client/components/ChatComponents/Emoticon.tsx
+++ client/components/ChatComponents/Emoticon.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import { StyleManifest } from '../../emoticonConfig/types/StyleManifest';
-
-// Unprintable character. Used to delimit emoticon information.
-const CONTROL_CODE_START = 0x001A;
-
-const EMOTION_BASE = 'assets/emo/';
-
-const metadataStyle: React.CSSProperties = {
-  display: 'none',
-};
+import { getEmoticonUrl } from '../../emoticonConfig';
 
 interface Props {
   unicode: number,
@@ -24,29 +16,8 @@ class Emoticon extends React.Component<Props> {
 
   constructor(props: Props | Readonly<Props>) {
     super(props);
-
     const { unicode, styleManifest, tone } = props;
-
-    // Some emoticon pack files are upper case, some lower case. The pack
-    // manifest tells us which.
-    const caseFn = styleManifest.lowerCase ? 'toLowerCase' : 'toUpperCase';
-
-    // Implement user tone preference if the emoticon supports it.
-    let emoticonSuffix: string;
-    if (tone && styleManifest.toneSupport.get(unicode)) {
-      emoticonSuffix =
-        styleManifest.tonePrefix + tone.toString(16)[caseFn]();
-    }
-    else {
-      emoticonSuffix = '';
-    }
-
-    // When concatenating with a few hundred strings, even fast computers start
-    // to crawl. This is a *far* faster method of string concatenation.
-    this.path = [
-      EMOTION_BASE, styleManifest.dir, '/', styleManifest.filePrefix || '',
-      unicode.toString(16)[caseFn](), emoticonSuffix, '.webp',
-    ].join('');
+    this.path = getEmoticonUrl(unicode, styleManifest, tone);
   }
 
   shouldComponentUpdate(nextProps: Readonly<Props>): boolean {
@@ -68,7 +39,7 @@ class Emoticon extends React.Component<Props> {
   };
 
   render() {
-    const { unicode, styleManifest, isPreview } = this.props;
+    const { unicode, styleManifest, tone, isPreview } = this.props;
 
     let style: React.CSSProperties;
     if (isPreview === false) {
@@ -88,17 +59,12 @@ class Emoticon extends React.Component<Props> {
     }
 
     return (
-      <span onClick={this.handleClick}>
-        <img
-          alt={String.fromCharCode(unicode)}
-          src={this.path}
-          style={style}
-        />
-
-        <span style={metadataStyle}>
-          {String.fromCharCode(CONTROL_CODE_START)}
-        </span>
-      </span>
+      <img
+        alt={String.fromCodePoint(unicode, tone)}
+        src={this.path}
+        style={style}
+        onClick={this.handleClick}
+      />
     );
   }
 }
