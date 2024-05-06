6c2020c696fb0437682281286ba6f476a2fe3f99
aggregate1166877 <noreply@frostoven.com>
2024-05-07 11:48:59 +1200

    Move emoticon URL resolver to emoticonConfig.

--- client/emoticonConfig/index.ts
+++ client/emoticonConfig/index.ts
@@ -10,6 +10,10 @@ import { tgramList, tgramToneSupport } from './2-Telegram/list';
 import { miscMojiList, miscMojiToneSupport } from './3-Misc/list';
 import { StyleManifest } from './types/StyleManifest';
 
+const EMOTION_BASE = 'assets/emo/';
+
+const emoticonPathCache = {};
+
 const emoticonConfig: StyleManifest[] = [
   {
     packId: '❄omj',
@@ -94,8 +98,43 @@ function getStyleById(packId: string) {
   return styleById[packId];
 }
 
+function getEmoticonUrl(unicode: number, styleManifest: StyleManifest, tone: number) {
+  // When I benchmarked string ops in the day, [].join() was literal orders of
+  // magnitude faster than concatenation (the computer would visibly freeze
+  // when concatenating many strings, but stay fluid with .join()). I see talk
+  // that things have since been optimized for the reverse.
+  // TODO: Test both methods on a few thousand emoticons loading all at once.
+  const cacheKey = [ styleManifest.packId, unicode, tone ].join('-');
+  if (emoticonPathCache[cacheKey]) {
+    return emoticonPathCache[cacheKey];
+  }
+
+  // Some emoticon pack files are upper case, some lower case. The pack
+  // manifest tells us which.
+  const caseFn = styleManifest.lowerCase ? 'toLowerCase' : 'toUpperCase';
+
+  // Implement user tone preference if the emoticon supports it.
+  let emoticonSuffix: string;
+  if (tone && styleManifest.toneSupport.get(unicode)) {
+    emoticonSuffix =
+      styleManifest.tonePrefix + tone.toString(16)[caseFn]();
+  }
+  else {
+    emoticonSuffix = '';
+  }
+
+  const path = [
+    EMOTION_BASE, styleManifest.dir, '/', styleManifest.filePrefix || '',
+    unicode.toString(16)[caseFn](), emoticonSuffix, '.webp',
+  ].join('');
+
+  emoticonPathCache[cacheKey] = path;
+  return path;
+}
+
 export {
   getStyleCount,
   getAllStyles,
   getStyleById,
+  getEmoticonUrl,
 };
