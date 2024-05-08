b6709d002668345f290a0a2721359abf02387473
aggregate1166877 <noreply@frostoven.com>
2024-05-08 20:53:21 +1200

    Change emoticon search index structure.

--- client/emoticonConfig/emoticonFinder.ts
+++ client/emoticonConfig/emoticonFinder.ts
@@ -3,54 +3,53 @@ import { EmoTrie } from './EmoTrie';
 
 let trieCache: EmoTrie;
 
-// Please do not use more than one word in the key. For example, if you want
-// 'tearsOfJoy' rather create one entry using 'tears' and another using 'joy'.
-// This is needed because we use a trie structure, which cannot search for
-// substrings (without pushing the complexity to O(n^2), anyway, thus making it
-// slower than a for loop). Also note the aliases that follow - for example, we
-// don't have a definition for 'cry', because 'cry' is an alias of 'sad'.
+// Please do not use more than one word in the a. For example, if you want
+// 'tearsOfJoy', rather create two entries: one for 'tears', and another for
+// 'joy'. This is needed because of how the emoticon search algorithm works.
 //
-// Also, please keep the keys in alphabetical order to ease maintenance. The
-// values on the right don't need to be alphabetical, those will display in the
-// order they're written.
-const emoticonAliases: { [key: string]: number[] } = {
-  aubergine: [ name.aubergine ],
-  broken: [ name.brokenHeart ],
-  cat: [ name.catFace ],
-  confused: [ name.faceConfused ],
-  folded: [ name.foldedHands ],
-  gun: [ name.pistol ],
-  grimace: [ name.faceGrimacing ],
-  hands: [ name.foldedHands, name.thumbsUp, name.thumbsDown ],
-  heart: [ name.heart, name.brokenHeart ],
-  monocle: [ name.faceWithMonocle ],
-  neutral: [ name.faceNeutral ],
-  peach: [ name.peach ],
-  thumbs: [ name.thumbsUp, name.thumbsDown ],
-  sad: [
-    name.faceSlightlyFrowning, name.faceConfused, name.faceNeutral,
-    name.faceWithDiagonalMouth, name.faceLoudlyCrying,
-    name.faceWithPleadingEyes, name.faceSmilingTear,
+// Also, for easier maintenance, please keep the keys in alphabetical order to
+// ease maintenance. Just beware that inherited values are in their own section
+// below the main one (for example, names.cry inherits from names.sad). The
+// values on the right don't need to be alphabetical, those will display in
+// the order they're written.
+const names: { [key: string]: number[] } = {};
+//
+names.aubergine = [ name.aubergine ];
+names.broken = [ name.brokenHeart ];
+names.cat = [ name.catFace ];
+names.confused = [ name.faceConfused ];
+names.folded = [ name.foldedHands ];
+names.gun = [ name.pistol ];
+names.grimace = [ name.faceGrimacing ];
+names.hands = [ name.foldedHands, name.thumbsUp, name.thumbsDown ];
+names.heart = [ name.heart, name.brokenHeart ];
+names.monocle = [ name.faceWithMonocle ];
+names.neutral = [ name.faceNeutral ];
+names.peach = [ name.peach ];
+names.sad = [
+  name.faceSlightlyFrowning, name.faceConfused, name.faceNeutral,
+  name.faceWithDiagonalMouth, name.faceLoudlyCrying, name.faceWithPleadingEyes,
+  name.faceSmilingTear,
+];
+names.salute = [ name.faceSalute ];
+names.smile = [
+  name.faceTearsOfJoy, name.faceSweatSmile, name.faceSlightlySmiling,
+  name.faceSmilingTear,
+];
+names.sweat = [ name.faceSweatSmile ];
+names.tear = [ name.faceTearsOfJoy, name.faceSmilingTear ];
+names.thinking = [ name.faceThinking, name.faceWithMonocle ];
+names.thumbs = [ name.thumbsUp, name.thumbsDown ];
 
-  ],
-  salute: [ name.faceSalute ],
-  smile: [
-    name.faceTearsOfJoy, name.faceSweatSmile, name.faceSlightlySmiling,
-    name.faceSmilingTear,
-  ],
-  sweat: [ name.faceSweatSmile ],
-  tear: [ name.faceTearsOfJoy, name.faceSmilingTear ],
-  thinking: [ name.faceThinking, name.faceWithMonocle ],
-};
-
-// Aliases
-emoticonAliases.cry = [ ...emoticonAliases.sad, name.faceTearsOfJoy ];
-emoticonAliases.eggplant = emoticonAliases.aubergine;
-emoticonAliases.laugh = emoticonAliases.smile;
-emoticonAliases.pistol = emoticonAliases.gun;
-emoticonAliases.thought = emoticonAliases.thinking;
-emoticonAliases.weapon = emoticonAliases.gun;
-emoticonAliases.water = emoticonAliases.gun;
+// Inherited values.
+names.cry = [ ...names.sad, name.faceTearsOfJoy ];
+names.eggplant = names.aubergine;
+names.joy = names.smile;
+names.laugh = names.smile;
+names.pistol = names.gun;
+names.thought = names.thinking;
+names.water = names.gun;
+names.weapon = names.gun;
 
 function findEmoticon(input: string, packId: string) {
   // Some performance.now() tests from my Ryzen 7 3750H laptop:
@@ -76,10 +75,10 @@ function findEmoticon(input: string, packId: string) {
   // taking the trie initialization into account.
   if (!trieCache) {
     trieCache = new EmoTrie();
-    const aliasNames = Object.keys(emoticonAliases);
+    const aliasNames = Object.keys(names);
     for (let i = 0, len = aliasNames.length; i < len; i++) {
       const alias = aliasNames[i];
-      const unicodeValues = emoticonAliases[alias];
+      const unicodeValues = names[alias];
       for (let j = 0, len = unicodeValues.length; j < len; j++) {
         const number = unicodeValues[j];
         trieCache.insert(alias, number);
